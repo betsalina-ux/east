1e61ed876e131be84aa6ce0ec590ac972047332a
betsalina-ux <betsalinan@gmail.com>
2026-05-23 08:19:40 -0700

    Fix transpile packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@deriv/core'],
-  output: 'standalone',
+  transpilePackages: ['@deriv/core', '@deriv-com/smartcharts-champion'],
 }
 
 module.exports = nextConfig
