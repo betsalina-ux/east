575667e99ba8a830811b12f66477f38ad182c65a
betsy <betsalinan@gmail.com>
2026-05-26 01:45:04 -0700

    Create page.tsx

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function AuthCallback() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const params = window.location.search;
+    router.replace('/' + params);
+  }, [router]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <p>Authenticating, please wait...</p>
+    </div>
+  );
+}
