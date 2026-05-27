8c876aa705915556c1ce2da2b0086600fd2acd94
betsy <betsalinan@gmail.com>
2026-05-27 03:13:53 -0700

    Update urls.ts

--- packages/core/src/config/urls.ts
+++ packages/core/src/config/urls.ts
@@ -12,12 +12,12 @@ const URLS = {
   production: {
     authBase: 'https://auth.deriv.com/oauth2',
     apiBase: 'https://api.derivws.com/trading/v1/options',
-    publicWs: 'wss://ws.derivws.com/websockets/v3?app_id=331ArfuZVDOjqzJpj764j',
+    publicWs: 'wss://ws.derivws.com/websockets/v3?app_id=1',
   },
   preview: {
     authBase: 'https://auth.deriv.com/oauth2',
     apiBase: 'https://staging-api.derivws.com/trading/v1/options',
-    publicWs: 'wss://ws.derivws.com/websockets/v3?app_id=331ArfuZVDOjqzJpj764j',
+    publicWs: 'wss://ws.derivws.com/websockets/v3?app_id=1',
   },
 } as const;
 
